67c5aa46aaf3c987bd01c453a44537d647621160
Miriam Rozenbaum <miriamrozenbaum@miriams-mbp.attlocal.net>
2018-11-13 08:51:56 -0500

    updated website styles

--- js/init.js
+++ js/init.js
@@ -11,3 +11,16 @@
 // initalization for navbar mobile/ small screens
 
 $(".button-collapse").sideNav();
+
+// initalization for collapsible accordion for projects page
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
